9f202df1086e8d33e8d072b5c72528cf1c0c9055
TheBlueBaron <ronniehughjames@protonmail.com>
2022-04-11 15:43:36 +0100

    Implemented card movement / reordering

--- src/Column.tsx
+++ src/Column.tsx
@@ -3,25 +3,29 @@ import { ColumnContainer, ColumnTitle } from "./styles"
 import { AddNewItem } from "./AddNewItem"
 import { useAppState } from "./state/AppStateContext"
 import { Card } from "./Card"
-import { moveList, addTask } from "./state/actions"
+import { moveList, addTask, moveTask, setDraggedItem } from "./state/actions"
 import { useItemDrag } from "./utils/useItemDrag"
 import { useDrop } from "react-dnd"
 import { isHidden } from "./utils/isHidden"
+import { DragItem } from "./DragItem"
 
 type ColumnProps = {
     text: string
     id: string
+    isPreview?: boolean
 }
 
-export const Column = ({ text, id}: ColumnProps) => {
+export const Column = ({ text, id, isPreview }: ColumnProps) => {
     const { draggedItem, getTasksByListId, dispatch } = useAppState()
 
     const tasks = getTasksByListId(id)
     const ref = useRef<HTMLDivElement>(null)
 
+    const { drag } = useItemDrag({ type: "COLUMN", id, text })
+
     const [, drop] = useDrop({
-        accept: "COLUMN",
-        hover() {
+        accept: ["COLUMN"],
+        hover() {            
             if (!draggedItem) {
                 return
             }
@@ -31,22 +35,21 @@ export const Column = ({ text, id}: ColumnProps) => {
                 }
 
                 dispatch(moveList(draggedItem.id, id))
-            }
+            }    
         }
-    })
-
-    const { drag } = useItemDrag({ type: "COLUMN", id, text })
+    })    
 
     drag(drop(ref))
 
     return (
-        <ColumnContainer 
+        <ColumnContainer
+            isPreview={isPreview}
             ref={ref}
-            isHidden={isHidden(draggedItem, "COLUMN", id)}
+            isHidden={isHidden(draggedItem, "COLUMN", id, isPreview)}
         >
             <ColumnTitle>{text}</ColumnTitle>
             {tasks.map(task => (
-                <Card text={task.text} key={task.id} id={task.id} />
+                <Card id={task.id} columnId={id} text={task.text} key={task.id} />
             ))}
             <AddNewItem
                 toggleButtonText="+ Add another task"
--- src/state/AppStateContext.tsx
+++ src/state/AppStateContext.tsx
@@ -38,18 +38,13 @@ const appData: AppState = {
     lists: [
         {
             id: "0",
-            text: "To Do",
-            tasks: [{ id: "c0", text: "Generate app scaffold" }]
+            text: "Example Column A",
+            tasks: [{ id: "c0", text: "Example Card A" }]
         },
         {
             id: "1",
-            text: "In Progress",
-            tasks: [{ id: "c2", text: "Learn Typescript" }]
-        },
-        {
-            id: "2",
-            text: "Done",
-            tasks: [{ id: "c3", text: "Begin to use static typing" }]
+            text: "Example Column B",
+            tasks: [{ id: "c2", text: "Example Card B" }]
         }
     ]
 }
